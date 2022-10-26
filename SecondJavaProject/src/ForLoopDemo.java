d96957e2d253f28e2cc1eb44e979b0086b107177
omkarnarveer <omkarnarveer07@gmail.com>
2022-10-26 12:31:48 +0530

    Version 7 ForLoopDemo class introduced

---
+++ SecondJavaProject/src/ForLoopDemo.java
@@ -0,0 +1,16 @@
+
+public class ForLoopDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//for(initalization;condition;increment/decrement)
+		for(int i=0;i<=5;i++) {
+		
+	}
+int j=1;
+for(;j<=5;) {
+	System.out.println("Hello "+j+" Iteration");
+	j++;
+}
+}
+}
