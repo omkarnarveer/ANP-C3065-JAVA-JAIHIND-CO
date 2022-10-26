b29f5e5d29abfc702ccb8c9075643c4b8b3c44f2
omkarnarveer <omkarnarveer07@gmail.com>
2022-10-26 12:58:04 +0530

    version 8 Updated added for loop in class ForLoopDemo

--- SecondJavaProject/src/ForLoopDemo.java
+++ SecondJavaProject/src/ForLoopDemo.java
@@ -5,12 +5,17 @@ public class ForLoopDemo {
 		// TODO Auto-generated method stub
 		//for(initialization;condition;increment/decrement)
 		for(int i=0;i<=5;i++) {
-		
+			if(i==2) 
+			continue;
+			System.out.println("Welcome "+i+" Iteration");
+		}
+		int j=0;
+		System.out.println("--------------------------------------");
+		for(;j<=5;) {
+			if(j==5)
+			break;
+			System.out.println("Hello "+j+" Iteration");
+			j++;
+		}
 	}
-int j=1;
-for(;j<=5;) {
-	System.out.println("Hello "+j+" Iteration");
-	j++;
-}
-}
 }
